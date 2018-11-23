e6ebff83bb880efde84a741e9cc66c0510a488bc
wongco <ginson@gmail.com>
2018-11-23 00:04:20 -0800

    refactor code for dynamic story entries

--- app.py
+++ app.py
@@ -6,14 +6,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
 debug = DebugToolbarExtension(app)
 
-# story_options = {
-#     "first_story":
-#     '''Once upon a time in a long-ago {place}, there lived a
-#        large {adjective} {noun}. It loved to {verb} {plural_noun}.''',
-#     "second_story":
-#     '''A long time ago in {place}, there was a microsized {adjective} {noun}. It wished to {verb} {plural_noun}.'''
-# }
-
 story_options = [
     {
         "title":
@@ -28,6 +20,13 @@ story_options = [
         "words": ["place", "noun", "verb", "adjective", "plural_noun"],
         "template":
         """A long time ago in {place}, there was a microsized {adjective} {noun}. It wished to {verb} {plural_noun}."""
+    },
+    {
+        "title":
+        "third_story",
+        "words": ["verb_past_tense", "adjective", "plural_noun"],
+        "template":
+        """Last year, there was a small {adjective} cookie. It {verb_past_tense} into several {plural_noun}."""
     }
 ]
 
@@ -35,16 +34,29 @@ story_options = [
 @app.route('/')
 def dropdown_form():
     """ dropdown form - creates down down option for story type - then sends you to inputs"""
-    # story_options is an iterable (ideally)
-    return render_template("dropdown.html", stories_options=story_options)
+
+    titles = [story["title"] for story in story_options]
+    return render_template("dropdown.html", titles=titles)
 
 
 @app.route('/form')
 def madlib_form():
     """ input forms for taking all the user information for the madlibs """
     current_story_title = request.args.get("title-selection")
+
+    # find index of story matching current story title
+    target_story_index = -1
+    for story in story_options:
+        if current_story_title == story.get("title"):
+            target_story_index = story_options.index(story)
+            break
+
+    request_words = story_options[target_story_index]["words"]
+
     return render_template(
-        "madlibform.html", current_story_title=current_story_title)
+        "madlibform.html",
+        current_story_title=current_story_title,
+        request_words=request_words)
 
 
 @app.route('/story')
@@ -54,10 +66,6 @@ def display_story():
     # grab current_story_selection from landing page
     current_story_title = request.args.get('current_story_title')
 
-    # defines instance of story type
-    # story = Story(["place", "noun", "verb", "adjective", "plural_noun"],
-    #               story_options[current_story_sel])
-
     # find index of story matching current story title
     target_story_index = -1
     for story in story_options:
@@ -65,20 +73,19 @@ def display_story():
             target_story_index = story_options.index(story)
             break
 
+    # defines instance of story type
     story = Story(story_options[target_story_index]["words"],
                   story_options[target_story_index]["template"])
 
-    # create the answers for our story
-    answer = {
-        "place": request.args.get('place'),
-        "noun": request.args.get('noun'),
-        "verb": request.args.get('verb'),
-        "adjective": request.args.get('adjective'),
-        "plural_noun": request.args.get('plural_noun')
-    }
+    # word template list of current story
+    template_words = story_options[target_story_index]["words"]
+
+    # dictionary comp - create the answers for our story
+    answer = {word: request.args.get(word) for word in template_words}
 
     # create story string from story generate method
-    result = story.generate(answer)
+    story_string = story.generate(answer)
 
     # render template with our story display html
-    return render_template("displaystory.html", complete_story_string=result)
+    return render_template(
+        "displaystory.html", complete_story_string=story_string)
